ceb64a4058073ebe00f74a6b448b173680facebe
Jepoy <poyhidalgo@gmail.com>
2025-06-27 22:11:36 +0800

    chore: Add axios for video upload functionality in Video component

--- app/(main)/video/paga.tsx
+++ app/(main)/video/paga.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Video() {
+  const [file, setFile] = useState<File | null>(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
+
+  const router = useRouter();
+
+  const MAX_FILE = 70 * 1024 * 1024;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!file) return;
+    if (file.size > MAX_FILE) {
+      console.log('File size exceeds the maximum limit of 70MB');
+      return;
+    }
+
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('originalSize', file.size.toString());
+
+    try {
+      const response = await axios.post('/api/video-upload', formData);
+
+      if (response.status === 200) {
+        console.log('File uploaded successfully');
+      } else {
+        console.log('File upload failed');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
   return (
-    <div>Video</div>
-  )
+    <div></div>
+  );
 }
